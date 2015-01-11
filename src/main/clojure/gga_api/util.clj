d2f1f0bf4c6b88aa91d2b33e33f18f8cd684bbcb
Matt Farmer <matt@frmr.me>
2015-01-11 12:24:04 -0500

    Add a db connection to the util namespace.

--- src/main/clojure/gga_api/util.clj
+++ src/main/clojure/gga_api/util.clj
@@ -1,8 +1,12 @@
 (ns gga-api.util
   (:require [noir.io :as io]
-            [markdown.core :as md]))
+            [markdown.core :as md]
+            [monger.core :as mg]))
 
 (defn md->html
   "reads a markdown file from public/md and returns an HTML string"
   [filename]
   (md/md-to-html-string (io/slurp-resource filename)))
+
+(def db (let [conn (mg/connect)]
+  (mg/get-db conn "galegis-api-dev")))
